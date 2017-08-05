b625a3be4b4a7ac09c57dfd40ecd4dec10aa503f
shinabarger <davidshinabarger@gmail.com>
2017-08-05 15:18:48 -0400

    add basic controller, as well as a separate class with constructors/getters & setters

--- src/main/java/com/example/springbootproject1/MainController.java
+++ src/main/java/com/example/springbootproject1/MainController.java
@@ -1,16 +1,21 @@
 package com.example.springbootproject1;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by davidshinabarger on 8/5/17.
  */
 
-@RequestMapping("/api")
+@RequestMapping("/")
 @RestController
 
 public class MainController {
 
-
+    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Widget testBasicSetup() {
+        return new Widget("Green", 1, 2);
+    }
 }
