aa5c8e263760516b0f712e060bd75eb1347f67e2
shinabarger <davidshinabarger@gmail.com>
2017-08-05 15:06:37 -0400

    Initial commit

---
+++ src/main/java/com/example/springbootproject1/MainController.java
@@ -0,0 +1,16 @@
+package com.example.springbootproject1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by davidshinabarger on 8/5/17.
+ */
+
+@RequestMapping("/api")
+@RestController
+
+public class MainController {
+
+
+}
